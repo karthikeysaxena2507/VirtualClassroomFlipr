4820efd9ea954839e7259109b782f67f6562ddfd
Karthikey <karthikeysaxena@gmail.com>
2021-08-29 00:54:39 +0530

    Added auth and loader

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -4,7 +4,7 @@ import { Spinner } from "react-bootstrap";
 const Loader = () => {
     return (<div className="text-center upper-margin"> 
         <span className = "mt-5"> <Spinner animation = "border" variant = "success" size = "lg"/> </span>
-    </div>)
+    </div>);
 };
 
 export default Loader;
